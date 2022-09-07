d05b2962f1531ab39f7bb5ed1ef27a220bad12cc
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2022-09-07 14:37:10 +0100

    bug in functional component -> IssueDelete.js

--- client/src/components/IssueDelete.js
+++ client/src/components/IssueDelete.js
@@ -1,28 +1,91 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Modal from "./Modal";
 import { Link } from "react-router-dom";
 import history from "../history";
-import { fetchIssue, deleteIssue } from "../actions";
-import { connect } from "react-redux";
+import { fetchIssue } from "../actions";
+// import { deleteIssue } from "../actions";
+// import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-class IssueDelete extends React.Component {
-  componentDidMount() {
-    this.props.fetchIssue(this.props.match.params.id);
-  }
+// class IssueDelete extends React.Component {
+//   componentDidMount() {
+//     this.props.fetchIssue(this.props.match.params.id);
+//   }
 
-  renderActions() {
-    // destructure variables
-    // const { id } is the same as this.props.match.params.id
-    const { id } = this.props.match.params;
-    console.log(id);
-    {
-      /* correctly passing the id to deleteIssue */
+//   renderActions() {
+//     // destructure variable
+//     const { id } = this.props.match.params;
+
+//     return (
+//       <React.Fragment>
+//         <button
+//           className="ui button negative"
+//           onClick={() => this.props.deleteIssue(id)}
+//         >
+//           Delete
+//         </button>
+//         <Link to="/" className="ui button">
+//           Cancel
+//         </Link>
+//       </React.Fragment>
+//     );
+//   }
+
+//   renderContent() {
+//     if (!this.props.issue) {
+//       return "Are you sure you want to delete this issue?";
+//     }
+//     return `Are you sure you want to delete this issue with title: ${this.props.issue.title} ?`;
+//   }
+
+//   render() {
+//     return (
+//       <Modal
+//         title="Delete Issue"
+//         content={this.renderContent()}
+//         actions={this.renderActions()}
+//         onDismiss={() => history.push("/")}
+//       />
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     issue: state.issues[ownProps.match.params.id],
+//   };
+// };
+
+// export default connect(mapStateToProps, { deleteIssue, fetchIssue })(
+//   IssueDelete
+// );
+
+//
+// ISSUEDELETE FUNCTIONAL COMPONENT
+//
+
+const IssueDelete = (props) => {
+  const dispatch = useDispatch();
+
+  const id = useSelector((state) => state.issues[props.match.params.id]);
+  const issue = useSelector((state) => state.issues[props.match.params.id]);
+
+  useEffect(() => dispatch(fetchIssue(id)));
+
+  const renderContent = () => {
+    // get title of item and display it
+    if (!issue) {
+      return "Are you sure you want to delete this issue?";
     }
+    return `Are you sure you want to delete: ${issue.title} ?`;
+  };
+
+  const renderActions = () => {
     return (
       <React.Fragment>
         <button
           className="ui button negative"
-          onClick={() => this.props.deleteIssue(id)}
+          onClick={() => dispatch({ type: "DELETE_ISSUE", payload: id })}
         >
           Delete
         </button>
@@ -31,33 +94,16 @@ class IssueDelete extends React.Component {
         </Link>
       </React.Fragment>
     );
-  }
-
-  renderContent() {
-    if (!this.props.issue) {
-      return "Are you sure you want to delete this issue?";
-    }
-    return `Are you sure you want to delete this issue with title: ${this.props.issue.title} ?`;
-  }
-
-  render() {
-    return (
-      <Modal
-        title="Delete Issue"
-        content={this.renderContent()}
-        actions={this.renderActions()}
-        onDismiss={() => history.push("/")}
-      />
-    );
-  }
-}
-
-const mapStateToProps = (state, ownProps) => {
-  return {
-    issue: state.issues[ownProps.match.params.id],
   };
+
+  return (
+    <Modal
+      title="Delete Issue"
+      content={renderContent()}
+      actions={renderActions()}
+      onDismiss={() => history.push("/")}
+    />
+  );
 };
 
-export default connect(mapStateToProps, { deleteIssue, fetchIssue })(
-  IssueDelete
-);
+export default IssueDelete;
