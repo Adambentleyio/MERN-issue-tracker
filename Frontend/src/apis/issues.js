d5fbca852b916c97e3390220f1d689933d9f602f
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2022-09-28 13:11:59 +0100

    change to baseurl for render.com server

--- Frontend/src/apis/issues.js
+++ Frontend/src/apis/issues.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // use axios with proxy uri set in package.json to ping local host in development
 export default axios.create({
-    baseURL: "http://localhost:5000" || 'https://ticketi.onrender.com'
+    baseURL: 'https://ticketi.onrender.com'
 });
