52925d82f0d7ad40c21f527bff00b9e9b0c66d32
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2022-09-28 12:23:14 +0100

    added backend url to frontend axios create

--- Frontend/src/apis/issues.js
+++ Frontend/src/apis/issues.js
@@ -1,4 +1,6 @@
 import axios from "axios";
 
 // use axios with proxy uri set in package.json to ping local host in development
-export default axios.create();
+export default axios.create({
+    baseURL: "http://localhost:5000" || 'https://ticketi.onrender.com'
+});
